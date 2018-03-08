460e389758a4d9d067c91fc9a47192a5aef017a8
jeremy.delferriere <jdelferriere@gmail.com>
2018-03-08 16:02:06 +0100

    005 - Added Booking

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -5,7 +5,7 @@ var HomeService = require('../services/home.service')
 class HomeController {
   getHome(req,res) {
     //HomeService.getHome(req.body).then(
-      res.render('home', { title: 'Express' })
+      res.render('home', { title: 'Mini Uber' })
     //);
   }
 }
